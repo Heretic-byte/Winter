4fe4d9db15dc0048a14b81c432dcc1d0fcd7a066
poiqwe <38466541+poiqwe96@users.noreply.github.com>
2022-02-26 10:19:07 +0900

    Clean WinterForest

---
+++ Source/Winter2/AI_Logic/Sensor_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Sensor_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WINTER2_API USensor_Base : public UObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Winter2/Managers/SpawnManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SpawnManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WINTER2_API USpawnManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
